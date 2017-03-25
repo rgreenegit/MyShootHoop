a12ba3300a49126ad30995ffca2415148cb06737
rgreenegit <richie.greene@gmail.com>
2017-03-25 15:31:57 +0000

    Inital Upload
    Adding To GitHub

---
+++ Assets/Scripts/PrimaryTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PrimaryTrigger : MonoBehaviour {
+    SecondaryTrigger SecondaryTrigger;
+    private void OnTriggerEnter(Collider collider)
+    {
+        //print(collider.gameObject.name);
+        if (collider.gameObject.name == "ball(Clone)")
+        {
+            SecondaryTrigger = GetComponentInChildren<SecondaryTrigger>();
+            SecondaryTrigger.expectCollision(collider.gameObject.GetInstanceID());
+        }
+    }
+}
---
+++ Assets/Scripts/SecondaryTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SecondaryTrigger : MonoBehaviour {
+    public int points = 1;
+    List<int> basketBalls = new List<int>(); //hold the IDs of the basketballs expected
+    
+    public void expectCollision(int instanceID)
+    {
+        basketBalls.Add(instanceID);
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        int basketBall = collider.gameObject.GetInstanceID();
+        if (basketBalls.Contains(basketBall))
+        {
+            ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper>();
+            scoreKeeper.IncrementScore(points);
+        }
+        else
+        {
+            print("Ball Unkown");
+        }
+    }
+}
+
